092e143f519a55185236256a019e7ec00d8ac64a
Dikontay <dalimbayeva@icloud.com>
2023-09-23 14:48:30 +0600

    added json

--- cmd/internal/main.go
+++ cmd/internal/main.go
@@ -9,8 +9,8 @@ import (
 func main() {
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./assets"))))
 	http.HandleFunc("/", routes.MainHandler)
-	http.HandleFunc("/game", routes.gameHandler)
-	http.HandleFunc("/map", routes.mapHandler)
+	// http.HandleFunc("/game", routes.gameHandler)
+	// http.HandleFunc("/map", routes.mapHandler)
 	fmt.Println("Server starts at: http://localhost:8080/")
 	http.ListenAndServe(":8080", nil)
 }
