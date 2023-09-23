fdf26b4ed5f599c6a99fcae55b7fed83ae164c1e
tyomnayark <tyomnayark@gmail.com>
2023-09-23 14:00:13 +0600

    add handlers

--- cmd/internal/main.go
+++ cmd/internal/main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
+	"tracker/routes"
+)
 
+func main() {
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./assets"))))
+	http.HandleFunc("/", routes.MainHandler)
+	http.HandleFunc("/game", routes.gameHandler)
+	http.HandleFunc("/map", routes.mapHandler)
+	fmt.Println("Server starts at: http://localhost:8080/")
+	http.ListenAndServe(":8080", nil)
 }
